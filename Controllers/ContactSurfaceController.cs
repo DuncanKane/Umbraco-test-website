4583a43dc250d708f00d065b4820a40a593417ff
Duncan Kane <47971814+DuncanKane@users.noreply.github.com>
2019-08-06 11:52:18 +0100

    Add files via upload

---
+++ Controllers/ContactSurfaceController.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Umbraco.Web.Mvc;
+using System.Web.Mvc;
+using TestWebCMS.Models;
+using System.Net.Mail;
+
+namespace TestWebCMS.Controllers
+{
+    public class ContactSurfaceController : SurfaceController
+    {
+        public const string PARTIAL_VIEW_FOLDER = "~/Views/Partials/Contact/";
+
+        public ActionResult RenderForm()
+        {
+            return PartialView(PARTIAL_VIEW_FOLDER + "_Contact.cshtml");
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult SubmitForm(ContactModel model)
+        {
+            if (ModelState.IsValid)
+            {
+                SendEmail(model);
+
+                return RedirectToCurrentUmbracoPage();
+            }
+            return CurrentUmbracoPage();
+        }
+
+        private void SendEmail(ContactModel model)
+        {
+            MailMessage message = new MailMessage(model.EmailAddress, "website@installumbraco.web.local");
+            message.Subject = string.Format("Enquiry from {0} {1} - {2}", model.FirstName, model.LastName, model.EmailAddress);
+            message.Body = model.Message;
+            SmtpClient client = new SmtpClient("127.0.0.1", 25);
+            client.Send(message);
+        }
+    }
+}
---
+++ Models/ContactModel.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel.DataAnnotations;
+
+namespace TestWebCMS.Models
+{
+    public class ContactModel
+    {
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string EmailAddress { get; set; }
+
+        [Required]
+        public string Message { get; set; }
+    }
+}
